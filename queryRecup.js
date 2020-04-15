6ee41293dc8afd420ca33d5cd92a4becdb7687af
bacar darwin <bacar.darwin.github@hotmail.com>
2020-04-15 11:55:14 +0200

    version affichage du resumer dans le panier

--- queryRecup.js
+++ queryRecup.js
@@ -116,6 +116,7 @@ console.log("à parti d'ici tout le code qui est en dessous fait partie de la pa
 
 function afficherDansLaPagePanier(){
     let cpt = 0;
+
     let resumePanier = document.querySelector(".resume");
     // recuperer les elements du tableaux
     /*                                                  */
@@ -124,15 +125,33 @@ function afficherDansLaPagePanier(){
         let req = new XMLHttpRequest(); 
         //fonction d'ecoute des requetes
             req.onreadystatechange = function(){
+                //ecoute de la requete
                 if(this.readyState == XMLHttpRequest.DONE && this.status == 200){
                     let reponsPanier = JSON.parse(this.response);
                     /*                                         */
                     console.log("####### test de mon algorithme ######");
-                    console.log(idRecuper);
+                    
                     console.log("**********************************");
                     console.log(idRecuper.id);
                     console.log("###### reponse reçu dans reponsPanier ######");
-                    console.log(reponsPanier);
+                    console.log(reponsPanier._id);
+                    console.log(reponsPanier.price);
+
+                        //fonction calcul du total
+                        function ajoutAuDom(){
+                            let eltAdd = reponsPanier;
+                            let eltDom = document.createElement("article");
+                            let eltImg = document.createElement("img");
+                            let eltName = document.createElement("h3");
+                            let eltPrice = document.createElement("span");
+                                eltImg.setAttribute("src", eltAdd.imageUrl);
+                                eltPrice.textContent = eltAdd.price;
+                                eltName.textContent = eltAdd.name;
+                                    eltDom.appendChild(eltImg); eltDom.appendChild(eltName); eltDom.appendChild(eltPrice);
+                                    resumePanier.appendChild(eltDom);
+                        }
+                        //
+                        ajoutAuDom();
                 }
 
             cpt++;
